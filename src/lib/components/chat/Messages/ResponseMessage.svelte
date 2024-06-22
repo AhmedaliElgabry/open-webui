3830213809b5cfa1a063f65e8679b8eae53ea147
Ahmed Elgabry <53542478+AhmedaliElgabry@users.noreply.github.com>
2024-06-22 12:39:45 +0300

    Update ResponseMessage.svelte

--- src/lib/components/chat/Messages/ResponseMessage.svelte
+++ src/lib/components/chat/Messages/ResponseMessage.svelte
@@ -353,7 +353,7 @@
 
 		<div class="w-full overflow-hidden pl-1">
 			<Name>
-				{`Fine Tunned Legal ${model?.name ?? message.model}`}
+				{`Legal ${model?.name ?? message.model}`}
 
 				{#if message.timestamp}
 					<span
